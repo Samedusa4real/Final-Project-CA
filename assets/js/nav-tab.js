8f493ae65b00dae24bd625eb652bfcb9898aee21
Samedusa4real <samadnm@code.edu.az>
2023-09-04 14:55:29 -0800

    all pages are ready for backend!

--- assets/js/nav-tab.js
+++ assets/js/nav-tab.js
@@ -1,9 +1,13 @@
-$('.user-nav-pills .nav-item .nav-link').on('click', function () {
-    setTimeout(
-        function () {
-            $("html, body").animate({
-                scrollTop: 0
-            }, 250);
-            return false;
-        }, 10);
-});
+export function navTab(){
+    $(".user-nav-pills .nav-item .nav-link").on("click", function () {
+      setTimeout(function () {
+        $("html, body").animate(
+          {
+            scrollTop: 0,
+          },
+          250
+        );
+        return false;
+      }, 10);
+    });
+}
---
+++ assets/js/pages/profile.js
@@ -0,0 +1,7 @@
+import { wizardJs } from "../wizard.js";
+import { addToCartButton } from "../add-to-cart.js"
+import { navTab } from "../nav-tab.js";
+
+wizardJs();
+addToCartButton();
+navTab();
