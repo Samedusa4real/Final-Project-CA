d7809307ace5678af6bbc6ba6db7bd3b7408aae4
Samedusa4real <samadnm@code.edu.az>
2023-09-24 13:38:50 -0800

    2 gun ara vermisdik ayga)

---
+++ Backend - Putka/Backend - Putka/Areas/Manage/Controllers/TagController.cs
@@ -0,0 +1,20 @@
+﻿using Backend___Putka.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend___Putka.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class TagController : Controller
+    {
+        private readonly PutkaDbContext _context;
+
+        public TagController(PutkaDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
